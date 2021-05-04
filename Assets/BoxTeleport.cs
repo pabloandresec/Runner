c4bbb99cba7235ca00626d8a4ebde0d41ba91cf4
pabloandrestdf <pabloandrestdf@gmail.com>
2021-05-04 20:16:28 -0300

    ZoomAndTeleportAnimations

--- Assets/BoxTeleport.cs
+++ Assets/BoxTeleport.cs
@@ -1,4 +1,5 @@
 ﻿using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,27 @@ using UnityEngine.SceneManagement;
 
 public class BoxTeleport : MonoBehaviour
 {
-    [SerializeField] private float animTime = 0.5f;
     [SerializeField] private Transform destiny;
     [SerializeField] private Transform tpPoint;
     [SerializeField] private Vector2 teleportExitVel = Vector2.zero;
     [SerializeField] private CinemachineVirtualCamera cam;
+    [Header("Times")]
+    [SerializeField] private float hideTime = 0.5f;
+    [SerializeField] private float restTime = 1f;
+    [SerializeField] private float moveAndLockToZoomInTGT = 0.5f;
+    [SerializeField] private float zoomInTime = 0.5f;
+    [SerializeField] private float revertCamOffsetAndZoomOutTGT = 0.5f;
     [Header("Audio")]
     [SerializeField] private AudioController audioController;
     [SerializeField] private int onTriggerEnter = 4;
     [Header("Next level")]
     [SerializeField] private bool teleportToNextLevel = false;
     [SerializeField] private int nextLevelBuildIndex = 2;
+    [Header("Animation Stuff")]
+    [SerializeField] private Transform zoomInPoint;
+    [SerializeField] private Transform zoomOutPoint;
     private bool warping = false;
+    private Rigidbody2D tgt;
     float ortGraphicSize = 5;
     private Vector3 ogOffset;
     private CinemachineFramingTransposer trans;
@@ -34,40 +44,62 @@ public class BoxTeleport : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-        if(rb.tag != "Player")
+        if(collision.CompareTag("Player"))
         {
-            return;
+            if (!warping)
+            {
+                tgt = collision.GetComponent<Rigidbody2D>();
+                Debug.Log("Box Teleporting start!");
+                OnBoxEnter?.Invoke();
+                warping = true;
+                tgt.GetComponent<Motor>().enabled = false;
+                tgt.isKinematic = true;
+                tgt.velocity = Vector2.zero;
+
+                audioController.PlaySFX(onTriggerEnter);
+
+                LeanTween.move(tgt.gameObject, tpPoint.position, hideTime).setOnComplete(() =>
+                {
+                    tgt.gameObject.SetActive(false);
+                    StartCoroutine(WaitAndExecute(restTime, MoveAndZoomToTGT));
+                });//move player to tpPoint tween
+            }
         }
-        if (rb != null && !warping)
-        {
-            OnBoxEnter?.Invoke();
-            warping = true;
-            rb.GetComponent<Motor>().enabled = false;
-            rb.isKinematic = true;
-            rb.velocity = Vector2.zero;
+    }
 
-            audioController.PlaySFX(onTriggerEnter);
+    private IEnumerator WaitAndExecute(float waitTime, Action v)
+    {
+        yield return new WaitForSeconds(waitTime);
+        v?.Invoke();
+    }
 
-            LeanTween.move(rb.gameObject, tpPoint.position, animTime);//move to tpPoint tween
-            LeanTween.value(cam.gameObject, v3 => trans.m_TrackedObjectOffset = v3, ogOffset, Vector3.zero, animTime * 0.9f); // offset tween
-            LeanTween.value(cam.gameObject, f => cam.m_Lens.OrthographicSize = f, ortGraphicSize, 0.1f, animTime + animTime * 0.05f).setOnComplete(() => { // zoom in tween
-                //Cuando se termine de hacer un zoom in a la caja
-                if(teleportToNextLevel)
+    private void MoveAndZoomToTGT()
+    {
+        LeanTween.move(tgt.gameObject, zoomInPoint.position, moveAndLockToZoomInTGT); // move disabled  player to zoom point
+        LeanTween.value(cam.gameObject, v3 => trans.m_TrackedObjectOffset = v3, ogOffset, Vector3.zero, moveAndLockToZoomInTGT);//Offset tween
+        LeanTween.value(cam.gameObject, f => cam.m_Lens.OrthographicSize = f, ortGraphicSize, 0.01f, moveAndLockToZoomInTGT).setOnComplete(() =>
+        { // zoom in tween
+          //Cuando se termine de hacer un zoom in a la caja
+            if (teleportToNextLevel)
+            {
+                SceneManager.LoadScene(nextLevelBuildIndex);
+            }
+            else
+            {
+                Teleport(tgt);
+                LeanTween.move(tgt.gameObject, zoomOutPoint.position, revertCamOffsetAndZoomOutTGT);
+                LeanTween.value(cam.gameObject, v3 => trans.m_TrackedObjectOffset = v3, Vector3.zero, ogOffset, revertCamOffsetAndZoomOutTGT); // offset tween
+                LeanTween.value(cam.gameObject, f => cam.m_Lens.OrthographicSize = f, 0.05f, ortGraphicSize, revertCamOffsetAndZoomOutTGT).setOnComplete(() => // zoom out tween
                 {
-                    SceneManager.LoadScene(nextLevelBuildIndex);
-                }
-                else
-                {
-                    Teleport(rb);
-                    LeanTween.value(cam.gameObject, v3 => trans.m_TrackedObjectOffset = v3, Vector3.zero, ogOffset, animTime * 0.9f); // offset tween
-                    LeanTween.value(cam.gameObject, f => cam.m_Lens.OrthographicSize = f, 0.05f, ortGraphicSize, animTime).setOnComplete(() => { // zoom out tween
-                                                                                                                                                //Cuando se termine de hacer un zoom out a la caja
+                    //Cuando se termine de hacer un zoom out a la caja
+                    StartCoroutine(WaitAndExecute(restTime, () =>
+                    {
+                        tgt.gameObject.SetActive(true);
                         warping = false;
-                    });
-                }
-            });
-        }
+                    }));
+                });
+            }
+        });
     }
 
     private void Teleport(Rigidbody2D rb)
