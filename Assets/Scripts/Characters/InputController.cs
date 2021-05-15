172fbb8075c190c29b78cb194ed9c7ff7c96bcfb
pabloandrestdf <pabloandrestdf@gmail.com>
2021-05-14 21:47:32 -0300

    Font fixed and build target swap

--- Assets/Scripts/Characters/InputController.cs
+++ Assets/Scripts/Characters/InputController.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class InputController : MonoBehaviour
 {
     [SerializeField] private Motor motor;
-
+    [SerializeField] float jumpCoolOff = 0.25f;
     private Vector2 input = Vector2.zero;
     private bool jump = false;
+    private bool canJump = true;
     private bool slide = false;
+    private float tPassed = 0;
 
     private void Start()
     {
@@ -23,6 +25,32 @@ public class InputController : MonoBehaviour
         if (Input.GetAxisRaw("Vertical") == -1)
         {
             slide = true;
+        }
+        if(Input.touchCount == 1)
+        {
+            Touch t = Input.GetTouch(0);
+            if (t.position.y > Screen.height / 2)
+            {
+                if(canJump)
+                {
+                    jump = true;
+                    canJump = false;
+                }
+            }
+            else
+            {
+                slide = true;
+            }
+        }
+
+        if(!canJump)
+        {
+            tPassed += Time.deltaTime;
+            if(tPassed > jumpCoolOff)
+            {
+                tPassed = 0;
+                canJump = true;
+            }
         }
     }
 
